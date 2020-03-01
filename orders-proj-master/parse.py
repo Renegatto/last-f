931a3d58ef6bf7f732675274f6f756d9f47927f4
savin <praise1234@mail.ru>
2020-03-02 00:21:36 +0300

    try to change color

--- orders-proj-master/parse.py
+++ orders-proj-master/parse.py
@@ -1,17 +1,19 @@
 
+
+import django
+from orders.models import Products
+
 import requests
 from bs4 import BeautifulSoup
 
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-import django
 django.setup()
 
 products = []
 product = {}
-
-from orders.models import Products
 
 
 def get_soup():
@@ -25,19 +27,24 @@ elems = soup.find_all("div", {'class': 'prod prod--toggle'})
 
 
 for elem in elems:
-    product['image'] = elem.find("img", {'class': 'prod__img'})['src']
-    product['name'] = elem.find("header", {'class': 'prod__head'}).get_text()
-    product['price'] = elem.find("a", {'class': 'money__val'}).get_text()
-    product['description'] = elem.find(
-        "p", {'class': 'prod__descr'}).get_text()
+    product = {
+        'image': elem.find_next("img", {'class': 'prod__img'})['src'],
+        'name': elem.find_next("header", {'class': 'prod__head'}).get_text(),
+        'price': elem.find_next("a", {'class': 'money__val'}).get_text(),
+        'description': elem.find_next(
+            "p", {'class': 'prod__descr'}).get_text()
+    }
     products.append(product)
-    print(products[0])
-    for p in products[0]:
-        pr = Products.objects.create()
-        pr.name = product['name']
-        pr.image = product['image']
-        pr.description = product['description']
-        pr.price = product['price']
-        pr.save()
+
+    # for product in products:
+    #     pr = Products.objects.create()
+    #     pr.name = product['name']
+    #     pr.image = product['image']
+    #     pr.description = product['description']
+    #     pr.price = product['price']
+    #     pr.save()
 
 
+for x in products[0:10]:
+    for item in x.items():
+        print(item)
